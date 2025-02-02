3df0c54992bf6046f3f3a742c689c41181bb9b7c
Akash Chaudhary <62457467+akash1902@users.noreply.github.com>
2025-02-02 21:07:06 +0530

    Add files via upload

---
+++ Assignment/verification-engine/dist/transactions/dto/parse-transaction.dto.d.ts
@@ -0,0 +1,3 @@
+export declare class ParseTransactionDto {
+    txHash: string;
+}
---
+++ Assignment/verification-engine/dist/transactions/dto/parse-transaction.dto.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ParseTransactionDto = void 0;
+class ParseTransactionDto {
+}
+exports.ParseTransactionDto = ParseTransactionDto;
+//# sourceMappingURL=parse-transaction.dto.js.map
